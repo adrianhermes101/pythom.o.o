78f69f581bb967806fe94d3412ee82ff2bd60615
yasmym <128698958+adrianhermes101@users.noreply.github.com>
2024-04-16 09:16:12 -0300

    Create atv1,5.py

---
+++ atv1,5.py
@@ -0,0 +1,20 @@
+class Carro:
+    def __init__(sel, marca, modelo, cor, ano)
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano 
+     
+    def exibir_informacoes();
+        print("Marca: ", self.marca)
+        print("Modelo: ", self.modelo)
+        print("Cor: ", self.cor)
+        print("Ano: ", self.ano)
+
+carro1 = Carro("Toyota", "Corolla", "Prata", 2002)
+carro1 = Carro("Honda", "Civic", "Preto", 2023)
+
+carro1.exibir_informacoes()
+carro2.exibir_informacoes()
+
+input()
